f5068df44fb5b727285c96454b0d970c1f342e73
hadiespace <v@hadiespace.ru>
2023-12-25 02:27:25 +0300

    2

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,10 +4,10 @@ config.setEnv();
 
 export const build = () => {
   console.log(config.isProd);
-  var
 };
 
 export const watch = () => {
   console.log(config.isProd);
-  console.log(config.isProd);
 };
+
+var
